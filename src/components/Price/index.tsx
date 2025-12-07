9ed1af88ed27ba423c882e750a709f8e5c528575
Danil Hutman <daniahutman@gmail.com>
2025-12-07 15:37:32 +0200

    fix price format

--- src/components/Price/index.tsx
+++ src/components/Price/index.tsx
@@ -3,11 +3,10 @@ import styles from "./index.module.css";
 
 type PriceProps = {
   price: number;
-  decimals?: number;
 };
 
-const Price: React.FC<PriceProps> = ({ price, decimals = 0 }) => (
-  <span className={styles.price}>{Number(price).toFixed(decimals)} $</span>
+const Price: React.FC<PriceProps> = ({ price }) => (
+  <span className={styles.price}>{Math.ceil(price)} $</span>
 );
 
 export default Price;
