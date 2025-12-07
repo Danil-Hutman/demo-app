ce7aaba97b043f7e96914082b8bd95a300366908
Danil Hutman <daniahutman@gmail.com>
2025-12-07 11:33:53 +0200

    add app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import styles from "./App.module.css";
+import Card from "./components/Card";
+import { getCactusList } from "./api";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [products, setProducts] = useState<
+    { id: number; name: string; description: string; price: number }[]
+  >([]);
+
+  useEffect(() => {
+    getCactusList()
+      .then(setProducts)
+      .catch(() => setProducts([]));
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className={styles.content}>
+      <header className={styles.header}>
+        <div>
+          <a href="/">
+            <h1>Product catalog</h1>
+          </a>
+        </div>
+        <div className={styles.brand}>
+          <p className={styles.brandText}>
+            Green companions <br />
+            for every space
+          </p>
+        </div>
+      </header>
+      <main>
+        <section className={styles.grid}>
+          {products.map((p) => (
+            <Card key={p.id} product={p} />
+          ))}
+        </section>
+      </main>
+    </div>
+  );
+};
 
-export default App
+export default App;
